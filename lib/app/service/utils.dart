1cc2ae1d63b3369b11a10ebae81c4063f1e5791c
Iqrom Abadi <iqromabadi15@gmail.com>
2024-08-02 22:00:26 +0700

    upcmong movie

--- lib/app/service/utils.dart
+++ lib/app/service/utils.dart
@@ -1,1 +1,2 @@
 const apiKey = "476aab979c17374aebf78d8a76b109fa";
+const imageUrl = 'https://tmdb.org/t/p/w500';
--- lib/app/widgets/bottom_nav_bar.dart
+++ lib/app/widgets/bottom_nav_bar.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/app/modules/home/views/home_view.dart';
+import 'package:netflix_clone/app/modules/new/views/new_view.dart';
+import 'package:netflix_clone/app/modules/search_movie/views/search_movie_view.dart';
 
-class BottomNav extends StatelessWidget {
-  const BottomNav({super.key});
+class BottomNavBar extends StatefulWidget {
+  BottomNavBar({super.key});
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> pages = [
+    HomeView(),
+    SearchMovieView(),
+    NewView(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_library_outlined),
+            label: 'New',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey[600],
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
